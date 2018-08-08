522e0e4e4685b3ccd8fe3a98330afef2a073568b
Feryadi <feryadialoi@gmail.com>
2018-08-09 00:33:15 +0700

    update modal cari buku icon

--- admin/page/peminjaman/modal_cari_buku.php
+++ admin/page/peminjaman/modal_cari_buku.php
@@ -9,7 +9,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title"><i class="material-icons">exit_to_app</i> Daftar Buku</h4>
+        <h4 class="modal-title"><i class="material-icons">book</i> Daftar Buku</h4>
       </div>
       <div class="modal-body">
 
